6e9bea58e582e1569931abb37be8d4b148163af5
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-05-31 22:07:58 +0800

    Update SMSUtils.java

--- src/main/java/com/skm/reggie/utils/SMSUtils.java
+++ src/main/java/com/skm/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tFbC915zm1LW685EuDe", "RB2yTuCr9vtDYPew43lG9IrhfKbw8e");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
